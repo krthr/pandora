a35787a3085b8637c8abcc789415dd82d1be6eaa
krthr <kartherrupnik@gmail.com>
2017-10-14 17:29:17 -0500

    Plantes

---
+++ assets/scripts/p5.js
@@ -0,0 +1,80 @@
+var planets,
+  theta = 0,
+  planet = "mars",
+
+  backgroundSound;
+
+function preload() {
+  // Información de los planetas
+  planets = {
+    sun: {
+      texture: loadImage("/images/textures/sun.jpg"),
+      size: 300,
+      gravity: 0,
+      sound: ''
+    },
+    mercury: {
+      texture: loadImage("/images/textures/mercury.jpg"),
+      gravity: 0
+    },
+    venus: {
+      texture: loadImage("/images/textures/venus.jpg"),
+      gravity: 0
+    },
+    earth: {
+      texture: loadImage("/images/textures/earth.jpg"),
+      gravity: 9.8
+    },
+    mars: {
+      texture: loadImage("/images/textures/mars.jpg"),
+      gravity: 0
+    },
+    jupiter: {
+      texture: loadImage("/images/textures/jupiter.jpg"),
+      gravity: 0
+    },
+    saturn: {
+      texture: loadImage("/images/textures/saturn.jpg"),
+      gravity: 0
+    },
+    uranus: {
+      texture: loadImage("/images/textures/uranus.jpg"),
+      gravity: 0
+    },
+    neptune: {
+      texture: loadImage("/images/textures/neptune.jpg"),
+      gravity: 0
+    }
+  }
+
+  soundFormats('mp3')
+  backgroundSound = loadSound('/sounds/interstellar.mp3')
+
+}
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.parent('app')
+  backgroundSound.loop()
+}
+
+function draw() {
+  background(0);
+  push();
+  var dirY = (mouseY / height - 0.5) * 4;
+  var dirX = (mouseX / width - 0.5) * 4;
+  directionalLight(200, 200, 200, dirX, dirY, 1);
+  rotateX(map(-1 * mouseX / 7, 0, height, 0, PI));
+  rotateY(theta);
+
+  texture(planets[planet].texture);
+
+  var size;
+  if (planets[planet].size) size = planets[planet].size;
+  else size = 150;
+  
+  sphere(size);
+
+  pop();
+  theta += 0.005;
+}
