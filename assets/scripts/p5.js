3e1f8f816ed0d859857876cbd62308f16bafc2a8
krthr <kartherrupnik@gmail.com>
2017-10-17 22:40:43 -0500

    Arreglos

--- assets/scripts/p5.js
+++ assets/scripts/p5.js
@@ -3,46 +3,70 @@ var planets = {
     name: 'Sol',
     size: 300,
     gravity: 274,
-    sound: ''
+    sound: '',
+    satelites: '-'
   },
   mercury: {
     name: 'Mercurio',
-    gravity: 2.8
+    gravity: 2.8,
+    satelites: 0,
+    day: 58.6,
+    year: 87.97
   },
   venus: {
     name: 'Venus',
-    gravity: 8.9
+    gravity: 8.9,
+    satelites: 0,
+    day: -243,
+    year: 224.7
   },
   earth: {
     name: 'Tierra',
-    gravity: 9.8
+    gravity: 9.8,
+    satelites: 1,
+    day: 23.93,
+    year: 365.25
   },
   mars: {
     name: 'Marte',
-    gravity: 3.7
+    gravity: 3.7,
+    satelites: 2,
+    day: 24.62,
+    year: 686.98
   },
   jupiter: {
     name: 'Júpiter',
     size: 270,
-    gravity: 22.9
+    gravity: 22.9,
+    satelites: 63,
+    day: 9.84,
+    year: 11.86
   },
   saturn: {
     name: 'Saturno',
-    gravity: 9.1
+    gravity: 9.1,
+    satelites: 61,
+    day: 10.23,
+    year: 29.46
   },
   uranus: {
     name: 'Urano',
-    gravity: 7.8
+    gravity: 7.8,
+    satelites: 27,
+    day: 17.9,
+    year: 84.01
   },
   neptune: {
     name: 'Neptuno',
-    gravity: 11.00
+    gravity: 11.00,
+    satelites: 13,
+    day: 16.11,
+    year: 164.8
   }
 }
 
 var theta = 0
-var backgroundSound
-
+var backgroundSound, guiSelect, startSound
 
 function preload() {
   // Cargar texturas
@@ -58,12 +82,15 @@ function preload() {
   soundFormats('mp3') // Formato de sonidos que trabajaremos
   // Cargar sonido de fondo
   backgroundSound = loadSound('/sounds/interstellar.mp3')
+  guiSelect = loadSound('/sounds/gui_select.mp3')
+  startSound = loadSound('/sounds/start.mp3')
 }
 
 function setup() {
   var canvas = createCanvas(windowWidth - 500, windowHeight, WEBGL) // Crear canvas
   canvas.parent('canvas') // El canvas se creará sobre el div#canvas
-  backgroundSound.loop() // Iniciar y repetir sonido de fondo
+  // backgroundSound.loop() // Iniciar y repetir sonido de fondo
+  startSound.loop()
 }
 
 function draw() {
